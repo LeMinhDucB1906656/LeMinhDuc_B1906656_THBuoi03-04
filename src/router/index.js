9d6540700bbc59f30656a62cccf9a0346c9f3d04
b1906656 <ducb1906656@student.ctu.edu.vn>
2022-11-03 08:24:28 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,12 @@ const routes = [
         name: "contactbook",
         component: ContactBook,
     },
+    {
+        path: "/contacts/:id",
+        name: "contact.edit",
+        component: () => import("@/views/ContactEdit.vue"),
+        props: true // Truyền các biến trong $route.params vào làm props
+        },
    
 ];
 const router = createRouter({
